978664631bdf4dd2c31e538267e93e3a727f7d16
juanAtomic <serratovjuandavid@gmail.com>
2024-02-06 20:32:14 -0500

    Completo

--- Inflación en Cali.R
+++ Inflación en Cali.R
@@ -11,7 +11,9 @@ colnames(Ipc_Cali)
 View(Ipc_Cali)
 
 # Gráfico multilineas comparando el IPC nacional vs IPC de Cali
-IPC<-ggplot(Ipc_Cali,aes(x= Periodo)) +  geom_line(aes(y = IPC_Cali), color = "blue", linetype = "solid", size = 1) + geom_line(aes(y = IPC_Col), color = "red", linetype = "solid", size = 1) + labs(title = "Comparación IPC nacional vs IPC Cali", x = "Periodo", y = "IPC")
+IPC<-ggplot(Ipc_Cali,aes(x= Periodo)) +  geom_line(aes(y = IPC_Cali, color = "IPC cali"), linetype = "solid", size = 1) + geom_line(aes(y = IPC_Col, color = "IPC colombia"), linetype = "solid", size = 1) + labs(title = "Comparación IPC nacional vs IPC Cali", x = "Periodo", y = "IPC")+ scale_color_manual(name="Leyenda",  values = c("IPC cali" = "blue", "IPC colombia" = "red"),labels = c("Cali", "Colombia"))
+
+print(IPC)
 
 #Calcular la inflación en cali y colombia
 
@@ -24,7 +26,9 @@ Ipc_Cali$Inflacion_colombia<-round((Ipc_Cali$IPC_Col-lag(Ipc_Cali$IPC_Col,1))/la
 
 #Gráfico multilineas comparando la inflacion en cali y colombia
 
-Inflacion<-ggplot(Ipc_Cali,aes(x= Periodo)) +  geom_line(aes(y = Inflacion_cali), color = "blue", linetype = "solid", size = 1) + geom_line(aes(y = Inflacion_colombia), color = "red", linetype = "solid", size = 1) + labs(title = "Comparación Inflación nacional vs Inflación Cali", x = "Periodo", y = "Inflación")
+Inflacion<-ggplot(Ipc_Cali,aes(x= Periodo)) +  geom_line(aes(y = Inflacion_cali, color = "Inflacion Cali"), linetype = "solid", size = 1) + geom_line(aes(y = Inflacion_colombia, color = "Inflacion colombia"), linetype = "solid", size = 1) + labs(title = "Comparación Inflación nacional vs Inflación Cali", x = "Periodo", y = "Inflación") + scale_color_manual(name="Leyenda",  values = c("Inflacion Cali" = "blue", "Inflacion colombia" = "red"),labels = c("Cali", "Colombia"))
+
+print(Inflacion)
 
 #Cambio del poder adquisitivo (tener en cuenta que se realiza con base al salario mínimo mensual establecido por ley)
 
@@ -36,7 +40,6 @@ Ipc_Cali$PA_Cali<-round(((lag(Ipc_Cali$IPC_Cali,1)/Ipc_Cali$IPC_Cali)-1)*100,2)
 
 Ipc_Cali$PA_Colombia<-round(((lag(Ipc_Cali$IPC_Col,1)/Ipc_Cali$IPC_Col)-1)*100,2)
 
-Poder_Adquisitivo<-ggplot(Ipc_Cali,aes(x= Periodo)) +  geom_line(aes(y = PA_Cali), color = "blue", linetype = "solid", size = 1) + geom_line(aes(y = PA_Colombia), color = "red", linetype = "solid", size = 1) + labs(title = "Comparación de la variación PA nacional vs variación de la PA Cali", x = "Periodo", y = "Variación de la PA")  
-
+Poder_Adquisitivo <- ggplot(Ipc_Cali, aes(x = Periodo)) +  geom_line(aes(y = PA_Cali, color = "PA Cali"), linetype = "solid", size = 1) + geom_line(aes(y = PA_Colombia, color = "PA Colombia"), linetype = "solid", size = 1) + labs(title = "Comparación de la variación PA nacional vs variación de la PA Cali", x = "Periodo", y = "Variación de la PA") +scale_color_manual(name="Leyenda",  values = c("PA Cali" = "blue", "PA Colombia" = "red"),labels = c("Cali", "Colombia"))
 print(Poder_Adquisitivo)
 
